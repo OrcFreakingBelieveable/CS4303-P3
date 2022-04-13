cfbe3665d95caa55f5f611d1f49925083ea68e6e
190021081 <gjc6@st-andrews.ac.uk>
2022-04-13 10:50:53 +0100

    Working hand drawn ellipses, and coloured shapes

--- Don't Drown/src/Sketcher.java
+++ Don't Drown/src/Sketcher.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PShape;
@@ -18,28 +20,24 @@ public abstract class Sketcher extends PApplet {
 
     /* Essentially creates a jagged quadrilteral to act as a hand-drawn line */
     protected PShape handDrawLine(PVector start, PVector end) {
-        if (start.x > end.x) {
-            PVector temp = start.copy();
-            start = end.copy();
-            end = temp;
-        }
-
         PVector smoothLine = start.copy().sub(end);
         float smoothLineLength = smoothLine.mag();
-
-        PVector topLeft = start.copy();
-        PVector topRight = end.copy();
         float heading = smoothLine.heading();
         heading += HALF_PI;
         PVector padding = PVector.fromAngle(heading).mult(strokeWeight);
+
+        // topLeft is not necessarily the top left corner, but it's easier to keep track
+        // of than just numbering the corners
+        PVector topLeft = start.copy().sub(padding);
+        PVector topRight = end.copy().sub(padding);
         PVector bottomRight = topRight.copy().add(padding);
         PVector bottomLeft = topLeft.copy().add(padding);
 
         PShape roughLine = createShape();
         roughLine.beginShape();
-        roughLine.vertex(start.x, start.y); // top left corner
+        roughLine.vertex(topLeft.x, topLeft.y); // top left corner
 
-        int sections = (int) random(0, shakiness * smoothLineLength);
+        int sections = (int) random(0, Math.max(2, shakiness * smoothLineLength));
         PVector section = topLeft.copy();
         PVector direction = (topRight.copy().sub(topLeft)).normalize();
         for (int i = 0; i < sections; i++) {
@@ -51,7 +49,7 @@ public abstract class Sketcher extends PApplet {
         roughLine.vertex(topRight.x, topRight.y);
         roughLine.vertex(bottomRight.x, bottomRight.y);
 
-        sections = (int) random(0, shakiness * smoothLineLength);
+        sections = (int) random(0, Math.max(2, shakiness * smoothLineLength));
         section = bottomRight.copy();
         direction = (bottomLeft.copy().sub(bottomRight)).normalize();
         for (int i = 0; i < sections; i++) {
@@ -65,7 +63,12 @@ public abstract class Sketcher extends PApplet {
         return roughLine;
     }
 
+    /* black stroke and empty fill */
     public PShape handDraw(int type, float... params) {
+        return handDraw(type, 0xFF000000, 0xFFFFFFFF, params);
+    }
+
+    public PShape handDraw(int type, int strokeColour, int fillColour, float... params) {
         switch (type) {
             case PConstants.QUAD:
                 // params : x1, y1, x2, y2, x3, y3, x4, y4
@@ -74,16 +77,79 @@ public abstract class Sketcher extends PApplet {
                     return createShape(type, params);
                 } else {
                     PShape quad = createShape(GROUP);
-                    quad.addChild(handDrawLine(new PVector(params[0], params[1]), new PVector(params[2], params[3])));
-                    quad.addChild(handDrawLine(new PVector(params[2], params[3]),
-                            new PVector(params[4], params[5])));
-                    quad.addChild(handDrawLine(new PVector(params[4], params[5]), new PVector(params[6], params[7])));
-                    quad.addChild(handDrawLine(new PVector(params[6], params[7]),
-                            new PVector(params[0], params[1])));
+
+                    PShape fill = createShape(type, params);
+                    fill.setFill(fillColour);
+                    quad.addChild(fill);
+
+                    PShape line = handDrawLine(new PVector(params[0], params[1]), new PVector(params[2], params[3]));
+                    line.setFill(strokeColour);
+                    quad.addChild(line);
+
+                    line = handDrawLine(new PVector(params[2], params[3]), new PVector(params[4], params[5]));
+                    line.setFill(strokeColour);
+                    quad.addChild(line);
+
+                    line = handDrawLine(new PVector(params[4], params[5]), new PVector(params[6], params[7]));
+                    line.setFill(strokeColour);
+                    quad.addChild(line);
+
+                    line = handDrawLine(new PVector(params[6], params[7]), new PVector(params[0], params[1]));
+                    line.setFill(strokeColour);
+                    quad.addChild(line);
+
                     return quad;
                 }
             case PConstants.RECT:
+                // params : x, y, width, height
+                if (params.length != 4) {
+                    System.err.println("handDraw(RECT) requires 4 floats in params");
+                    return createShape(type, params);
+                } else {
+                    return handDraw(PConstants.QUAD, strokeColour, fillColour,
+                            params[0], params[1],
+                            params[0] + params[2], params[1],
+                            params[0] + params[2], params[1] + params[3],
+                            params[0], params[1] + params[3]);
+                }
             case PConstants.ELLIPSE:
+                // params : vertices, x, y, width, height
+                if (params.length != 5) {
+                    System.err.println("handDraw(ELLIPSE) requires 5 floats in params");
+                    return createShape(type, Arrays.copyOfRange(params, 1, 4));
+                } else {
+                    int vertices = (int) params[0];
+                    float centreX = params[1];
+                    float centreY = params[2];
+                    float width = params[3] / 2;
+                    float height = params[4] / 2;
+
+                    PShape ellipse = createShape(GROUP);
+
+                    // add the fill as a normal shape
+                    PShape fill = createShape(type, centreX, centreY, width * 2, height * 2);
+                    fill.setFill(fillColour);
+                    ellipse.addChild(fill);
+
+                    // generate the lines around the circumference of the ellipse
+                    PVector start = new PVector(centreX + width * cos(0), centreY + height * sin(0));
+                    PVector end = new PVector();
+
+                    for (int i = 1; i <= vertices; i++) {
+                        float angle = i * TAU / vertices;
+                        end.x = (centreX + width * cos(angle));
+                        end.y = (centreY + height * sin(angle));
+
+                        PShape line = handDrawLine(start, end);
+                        line.setFill(strokeColour);
+                        ellipse.addChild(line);
+
+                        start.x = end.x;
+                        start.y = end.y;
+                    }
+
+                    return ellipse;
+                }
             default:
                 System.err.println("handDraw() only works with QUAD, RECT, and ELLIPSE objects");
                 return createShape(type, params);
--- Don't Drown/src/Test.java
+++ Don't Drown/src/Test.java
@@ -8,33 +8,39 @@ public class Test extends Sketcher {
     int frame = 0;
 
     public Test() {
-        super(3f, 0.2f, 0.05f);
+        super(3f, 0.3f, 0.05f);
     }
 
     @Override
     public void settings() {
-        size(400, 400);
+        size(1024, 768);
     }
 
     @Override
     public void draw() {
-        background(0xFFFFFFFF);
+        background(0xFFBBBBBB);
+        stroke(255);
         noStroke();
         fill(0xFF000000);
-        if (frame++ % 60 == 0) {
+        if (frame++ % 20 == 0) {
             s1 = handDrawLine(new PVector(100f, 100f), new PVector(200f, 100f));
             s2 = handDrawLine(new PVector(200f, 100f - strokeWeight), new PVector(200f, 200f));
             s3 = handDrawLine(new PVector(200f, 200f), new PVector(100f, 200f));
             s4 = handDrawLine(new PVector(100f, 200f), new PVector(100f, 100f - strokeWeight));
 
-           // s5 = handDraw(QUAD, 100f, 100f, 200f, 100f, 200f, 200f, 100f, 200f);
-            s5 = handDraw(QUAD, 100f, 150f, 150f, 100f, 200f, 150f, 150f, 200f);
+            s5 = handDraw(QUAD, 100f, 100f, 200f, 100f, 200f, 200f, 100f, 200f);
+            // s5 = handDraw(QUAD, 0xFF000000, 0xFFFF0066, 100f, 150f, 150f, 100f, 200f, 150f, 150f, 200f);
+            // s5 = handDraw(RECT, 0xFF000000, 0xFFFF0066, 100f, 100f, 100f, 200f);
+            // s5 = handDraw(ELLIPSE, 0xFF000000, 0xFFFFFF00, 40, width / 2, height / 2, 150f, 150f);
         }
-       /* shape(s1);
-        shape(s2);
-        shape(s3);
-        shape(s4);*/
+        /*
+         * shape(s1);
+         * shape(s2);
+         * shape(s3);
+         * shape(s4);
+         */
         shape(s5);
+        text((int)frameRate, 50, 50);
     }
 
     public static void main(String[] args) {
