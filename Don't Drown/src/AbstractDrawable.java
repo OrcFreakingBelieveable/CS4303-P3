4920c38802a7b4fa154bdcd5d023ad4b1c6cccaf
190021081 <gjc6@st-andrews.ac.uk>
2022-04-18 15:50:53 +0100

    Removed TODOs

--- Don't Drown/src/AbstractDrawable.java
+++ Don't Drown/src/AbstractDrawable.java
@@ -2,9 +2,6 @@ import processing.core.PShape;
 import processing.core.PVector;
 
 public abstract class AbstractDrawable {
-    // TODO make all objects semi-transparent 
-    // TODO statically define my two colour modes 
-
     protected final DontDrown sketch;
     protected final LevelState state;
 
